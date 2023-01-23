89252782ea397f553cf021a73a0f46b1753edad0
IsabelleTancred <isabelleetancred@gmail.com>
2023-01-23 14:37:30 +0100

    Feat: sync server and UI

--- src/views/TranslationPage.jsx
+++ src/views/TranslationPage.jsx
@@ -1,27 +1,33 @@
 import { translationAdd } from "../api/translate";
 import TranslationButton from "../components/Translation/TranslationButton";
 import TranslationForm from "../components/Translation/TranslationForm";
+import { STORAGE_KEY_USER } from "../const/storageKeys";
 import { useUser } from "../context/UserContext";
 import withAuth from "../hoc/withAuth";
+import { storageSave } from "../utils/storage";
 
 const Translate = () => {
-    const { user } = useUser();
-    const handleTranslationClicked = async (text) => {
-        const [error, result] = await translationAdd(user, text)
-        console.log(text)
-        console.log('error', error)
-        console.log('resul', result)
+  const { user, setUser } = useUser();
+  const handleTranslationClicked = async (text) => {
+    const [error, updatedUser] = await translationAdd(user, text);
+    if (error !== null) {
+      return;
     }
-    return(
-    <> 
-        <h1>Translate</h1> 
-        <section id="translate-button">
-            <TranslationButton name="Translate" key="translate"/>
-        </section>
-        <section id="translate2">
-            <TranslationForm onTranslation={ handleTranslationClicked }/>
-        </section>
+    storageSave(STORAGE_KEY_USER, updatedUser);
+    setUser(updatedUser);
+    console.log("Error", error);
+    console.log("UpdatedUser", updatedUser);
+  };
+  return (
+    <>
+      <h1>Translate</h1>
+      <section id="translate-button">
+        <TranslationButton name="Translate" key="translate" />
+      </section>
+      <section id="translate2">
+        <TranslationForm onTranslation={handleTranslationClicked} />
+      </section>
     </>
-    )
-}
+  );
+};
 export default withAuth(Translate);
